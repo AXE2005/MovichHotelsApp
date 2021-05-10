c4204f984d8f82fafeb7919ad7defec821097473
AxE <josephcoelo@hotmail.com>
2021-05-10 09:02:20 -0500

    Start

---
+++ www/js/main.js
@@ -0,0 +1,174 @@
+//switch
+$(".switch").click(function () {
+	// Simple Code
+	var self = $(this);
+
+	if (self.hasClass("on")) {
+		self.removeClass("on");
+	} else {
+		self.addClass("on");
+	}
+});
+
+// buttons
+$("#open-door").click(function () {
+	alert("open-door");
+});
+
+// buttons - temperature
+
+$("#power-temp").click(function () {
+	alert("power temp");
+});
+
+$("#fan-left").click(function () {
+	alert("fan-left");
+});
+$("#fan-right").click(function () {
+	alert("fan-right");
+});
+$("#temperature-left").click(function () {
+	alert("temperature-left");
+});
+$("#temperature-right").click(function () {
+	alert("temperature-right");
+});
+$("#swing").click(function () {
+	alert("swing");
+});
+$("#slep-temperature").click(function () {
+	alert("slep-temperature");
+});
+
+// BOTONES SMARTTV
+
+$("#volplus").click(function () {
+	alert("volplus ");
+});
+
+$("#power").click(function () {
+	alert("power");
+});
+
+$("#up").click(function () {
+	alert("up ");
+});
+
+$("#chplus").click(function () {
+	alert("chplus");
+});
+
+$("#vol").click(function () {
+	alert("vol ");
+});
+
+$("#left").click(function () {
+	alert("left ");
+});
+
+$("#ok").click(function () {
+	alert("ok ");
+});
+
+$("#right").click(function () {
+	alert("right ");
+});
+
+$("#ch").click(function () {
+	alert("ch ");
+});
+$("#volmin").click(function () {
+	alert("volmin ");
+});
+
+$("#down").click(function () {
+	alert("down ");
+});
+
+$("#chmin").click(function () {
+	alert("chmin ");
+});
+
+$("#mute").click(function () {
+	alert("mute ");
+});
+
+$("#guide").click(function () {
+	alert("guide ");
+});
+
+// BOTONES SOUND
+
+$("#power-sound").click(function () {
+	alert("power-sound ");
+});
+
+$("#left-volumen-sound").click(function () {
+	alert("left-volumen-sound ");
+});
+
+$("#right-volumen-sound").click(function () {
+	alert("right-volumen-sound ");
+});
+
+$("#left-tuner-sound").click(function () {
+	alert("left-tuner-sound ");
+});
+
+$("#right-tuner-sound").click(function () {
+	alert("right-tuner-sound ");
+});
+
+$("#right-input-sound").click(function () {
+	alert("right-input-sound ");
+});
+
+$("#left-sleep-sound").click(function () {
+	alert("left-sleep-sound ");
+});
+
+$("#right-sleep-sound").click(function () {
+	alert("right-sleep-sound ");
+});
+
+// BOTONES INTERACTIVE TV
+
+$("#up-int-tv").click(function () {
+	alert("up-int-tv ");
+});
+
+$("#left-int-tv").click(function () {
+	alert("left-int-tv ");
+});
+
+$("#ok-int-tv").click(function () {
+	alert("ok-int-tv ");
+});
+
+$("#right-int-tv").click(function () {
+	alert("right-int-tv ");
+});
+
+$("#down-int-tv").click(function () {
+	alert("down-int-tv ");
+});
+
+// BOTONES LUCES
+
+// BOTONES CORTINAS HABITACION
+
+$("#abrir-cortina").click(function () {
+	alert("abrir-cortina ");
+});
+
+$("#stop-abrir-cortina").click(function () {
+	alert("stop-abrir-cortina ");
+});
+
+$("#cerrar-cortina").click(function () {
+	alert("cerrar-cortina ");
+});
+
+$("#stop-cerrar-cortina").click(function () {
+	alert("stop-cerrar-cortina ");
+});
